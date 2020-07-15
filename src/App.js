f42bdfd28793ab6d43c57a3eb90e63768d904799
Uzair Iqbal <uzair.qq@outlook.com>
2020-07-15 17:02:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Uzair
+          Learn React With Uzair iqbal
         </a>
       </header>
     </div>
